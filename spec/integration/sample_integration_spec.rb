9980ab1992f19561b163479700afefb3d64b5274
egabrielsen <erik.gabe@gmail.com>
2016-03-25 15:07:41 -0500

    working on integration tests

--- spec/integration/sample_integration_spec.rb
+++ spec/integration/sample_integration_spec.rb
@@ -22,17 +22,49 @@ describe 'Table', type: :feature do
       expect(page).to have_content('Email')
     end
 
-    it 'should be empty' do
-      visit '/'
-      fields = all(:css, '.form-control')
-      expect(fields[0].value).to eq('')
-      fields[0].set('Bob')
+    describe 'search bar' do
+      it 'should show search help' do
+        visit '/'
+        find('.glyphicon-search').click
+        expect(page).to have_content('Search Help')
+        expect(page).to have_content('first:')
+        expect(page).to have_content('last:')
+        expect(page).to have_content('email:')
+      end
+
+      it 'should be empty' do
+        visit '/'
+        fields = all(:css, '.form-control')
+        expect(fields[0].value).to eq('')
+      end
+
+      it 'should accept OR queries' do
+        visit '/'
+        fields = all(:css, '.form-control')
+        fields[0].set('Bob | Jerry')
+        # expect correct rows to be shown
+        # expect to not see Mary
+      end
+
+      it 'should accept AND queries' do
+        visit '/'
+        fields = all(:css, '.form-control')
+        fields[0].set('Bob, Smith')
+        # expect correct rows to be shown
+      end
+
+      it 'should accept field queries' do
+        visit '/'
+        fields = all(:css, '.form-control')
+        fields[0].set('Bob, email:Bob.Barker@sample.com')
+        # expect correct rows to be shown
+      end
     end
 
-    it 'should show query inputs' do
-      visit '/'
-      find('.glyphicon-search').click
-      expect(page).to have_content('Search Help')
+    describe 'filtering' do
+      it 'should filter '
     end
+
+
   end
 end
